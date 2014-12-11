bc0485151f878dd33685aaadc392ac98b4e52fcb
Evgeny Borisov <q33company@gmail.com>
2014-12-11 11:06:54 +0300

    Init

--- Cpp/Game of Life/main.cpp
+++ Cpp/Game of Life/main.cpp
@@ -12,6 +12,13 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+struct point
+{
+	int x;
+	int y;
+	int col;
+};
+
 bool init();
 bool loadMedia();
 void close();
@@ -130,10 +137,10 @@ SDL_Texture* loadTexture( std::string path )
 void pointer (int x, int y, int col) {
     SDL_Rect fillRect = { x ,y,3,3};
     if (col == 1){
-            SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+            SDL_SetRenderDrawColor( gRenderer, 255, 255, 255, 255 );
     }
     else{
-        SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+        SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 0 );
     }
     SDL_RenderFillRect( gRenderer, &fillRect );
 }
